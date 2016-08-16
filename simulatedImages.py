54df7ae3ca8367a4ddde44ad14410450248a9ea7
Bodeeen <andreas.boden@gmail.com>
2016-08-16 11:41:58 +0200

    Added addNoise and addNoiseStack and minor change in rawStack.

--- simulatedImages.py
+++ simulatedImages.py
@@ -11,6 +11,7 @@ from scipy import ndimage as ndi
 from PIL import Image
 import scipy.signal
 import tifffile
+import time
 
 def makeGaussian(size, fwhm = 3, center=None):
     """ Make a square gaussian kernel.
@@ -21,7 +22,7 @@ def makeGaussian(size, fwhm = 3, center=None):
     """
 
     x = np.arange(0, size, 1, float)
-    y = x[:,np.newaxis]
+    y = x[:, np.newaxis]
 
     if center is None:
         x0 = y0 = size // 2
@@ -44,7 +45,7 @@ def periodicMatrix(size, period, fwhm, xoffset, yoffset):
     return M
     
     
-def circlesMatrix(size, N):
+def circlesMatrix(size, N, noise):
     """ Makes a matrix with many circles of random centres, it is the image
     to evaluate resolution"""
     
@@ -57,29 +58,70 @@ def circlesMatrix(size, N):
         donut = (circle < int(size/10+size/500)**2) & (circle > int(size/10-size/500)**2)
         donut = np.array(donut, dtype='int')
         M = M+donut
+    return 1000*M
+
+def addNoise(gtruth, offset, nfac):
+        imsize = np.shape(gtruth)[0]
+        offsetimg = np.zeros([imsize, imsize]) + offset + 4*np.random.randn(imsize, imsize)
+        poissonian = np.random.poisson(nfac*gtruth, [imsize, imsize])
+        out = offsetimg + poissonian
         
-    return M
+        return out
+        
+def addNoiseStack(stack, offset, nfac):
+    frames = np.shape(stack)[0]
+    noised = []
+    for i in reversed(np.arange(frames)):
+        frame = stack[i]
+        noised.append(addNoise(frame, 110, nfac))
+        
+    return noised
+        
+#def rawStack(image, period, fwhm):
+#    """ Generates simulated parallelized RESOLFT raw data stack """
+#    imageSize = np.shape(image)[0]
+#    stack = []
+#    for i in np.arange(32):
+#        for j in np.arange(32):
+#            frame = periodicMatrix(imageSize, period, fwhm, i, j)*image
+#            frame = (10**4)*ndi.gaussian_filter(frame, 5)
+#            stack.append(frame)
+#    return stack
 
 
-    
-def rawStack(image):
+def rawStack(image, period, fwhm, step_size):
     """ Generates simulated parallelized RESOLFT raw data stack """
+    imageSize = np.shape(image)[0]
+    pattern = periodicMatrix(imageSize, period, fwhm, 0, 0)
     stack = []
-    for i in np.arange(16):
-        for j in np.arange(16):
-            frame = periodicMatrix(500, 16, 2.5,i,j)*circles
-            frame = (10**4)*ndi.gaussian_filter(frame, 5)
+    
+    # scan is done first right, then down
+    for i in np.arange(period/step_size):
+        for j in np.arange(period/step_size):
+            dx = i*step_size
+            dy = j*step_size
+            frame = pattern*scipy.ndimage.interpolation.shift(image, [dx, dy])
+            frame = ndi.gaussian_filter(frame, 5)
             stack.append(frame)
     return stack
 
+size = 400
+offset = 110
+noise = 0.5
 
-circles = circlesMatrix(500, 56)
-circles = np.array(10*circles, dtype='uint16')
-tifffile.imsave('circles.tif', circles)
+circles = circlesMatrix(size, 56, noise)
+circles = np.array(circles, dtype='uint16')
+noisedCirc = addNoise(circles, offset, noise)
+noisedCirc = np.array(noisedCirc, dtype='uint16')
+tifffile.imsave('circles_2period.tif', circles)
 
-rawstack = rawStack(circles)
-rawstack = np.array(rawstack,dtype='uint16')
-tifffile.imsave('rawstack.tif', rawstack)
+darkframe = addNoise(np.ones((size, size)), offset, noise)
+darkframe = np.array(darkframe, dtype='uint16')
+tifffile.imsave('darkframe.tif', darkframe)
+
+rawstack = rawStack(circles, 16, 8, 0.5)
+rawstack = np.array(addNoiseStack(rawstack, offset, noise),dtype='uint16')
+tifffile.imsave('rawstack1_p.tif', rawstack)
 
 
 
@@ -89,7 +131,7 @@ tifffile.imsave('rawstack.tif', rawstack)
 #plt.imshow(activated,interpolation='none',cmap='hot')
 #
 #plt.figure()
-#plt.imshow(circles,interpolation='none',cmap='hot')
+#plt.imshow(scipy.ndimage.interpolation.shift(circles,[30,0]),interpolation='none',cmap='hot')
 #
 #plt.figure()
 #plt.imshow(rawdata,interpolation='none',cmap='hot')
